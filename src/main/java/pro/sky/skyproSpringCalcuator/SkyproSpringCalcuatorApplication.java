d925b803f77ed867d785d8e7758482b45dc73900
Unteryana <ishtar_v@bk.ru>
2023-05-28 17:54:51 +0300

    initial commit

---
+++ src/main/java/pro/sky/skyproSpringCalcuator/SkyproSpringCalcuatorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproSpringCalcuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringCalcuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringCalcuatorApplication.class, args);
+	}
+
+}
